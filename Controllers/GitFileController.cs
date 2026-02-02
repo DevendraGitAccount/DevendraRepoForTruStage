c0c22a6bc3984d888a4e9b530cbac5be0953fad8
Devendra Kumar <Devendra.Kumar@trustage.com>
2026-02-02 19:58:42 +0530

    Added file

---
+++ Controllers/GitFileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaveFileInGitPOC.Models;
+using SaveFileInGitPOC.Services;
+
+namespace SaveFileInGitPOC.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GitFileController
+    {
+        [HttpPost("savefile")]
+        public string SaveFile([FromBody] FileCommitRequest request)
+        {
+            try
+            {
+                //_gitService.SaveAndCommitFile(request.FileName, request.Content, request.CommitMessage);
+                return "File saved, committed, and pushed successfully.";
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                return "An error occurred: {ex.Message}";
+            }
+        }
+    }
+}
