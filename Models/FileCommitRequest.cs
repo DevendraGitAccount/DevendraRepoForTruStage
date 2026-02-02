fa4e0e114267d164e11b5548a85cb15e8b36cdab
Devendra Kumar <Devendra.Kumar@trustage.com>
2026-02-02 19:23:49 +0530

    file added

---
+++ Models/FileCommitRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SaveFileInGitPOC.Models
+{
+    public class FileCommitRequest
+    {
+        public string FileName { get; set; }
+        public string Content { get; set; }
+        public string CommitMessage { get; set; }
+    }
+}
